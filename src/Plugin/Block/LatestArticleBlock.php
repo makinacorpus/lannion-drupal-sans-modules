82c127bcf23e6e9e43d357b2385453ec4d993dc5
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-10-17 17:17:03 +0200

    latest article block plugin polishing

--- src/Plugin/Block/LatestArticleBlock.php
+++ src/Plugin/Block/LatestArticleBlock.php
@@ -3,6 +3,9 @@
 namespace MakinaCorpus\Lannion\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Entity\EntityManagerInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * @Block(
@@ -11,15 +14,55 @@ use Drupal\Core\Block\BlockBase;
  *   category = @Translation("Articles"),
  * )
  */
-class LatestArticleBlock extends BlockBase
+class LatestArticleBlock extends BlockBase implements ContainerFactoryPluginInterface
 {
+    private $entityManager;
+
+    /**
+     * Constructs a new SystemMenuBlock.
+     */
+    public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityManagerInterface $entityManager)
+    {
+        parent::__construct($configuration, $plugin_id, $plugin_definition);
+
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+    {
+        return new static($configuration, $plugin_id, $plugin_definition, $container->get('entity.manager'));
+    }
+
     /**
      * {@inheritdoc}
      */
     public function build()
     {
-        return [
-            '#markup' => $this->t('Hello, World!'),
-        ];
+        $storage = $this->entityManager->getStorage('node');
+
+        $idList = $storage
+            ->getQuery()
+            ->condition('status', 1)
+            ->sort('created', 'desc')
+            ->range(0, 1)
+            ->addTag('node_access')
+            ->execute()
+        ;
+
+        if (!$idList) {
+            return ['#markup' => '<p>Ce site est désespérement vide&nbsp;!</p>'];
+        }
+
+        return $this
+            ->entityManager
+            ->getViewBuilder('node')
+            ->viewMultiple(
+                $storage->loadMultiple($idList),
+                'sidebar_block'
+            )
+        ;
     }
 }
