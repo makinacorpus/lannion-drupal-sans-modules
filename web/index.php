4d0437788b7ca55713ea88c70ecd1deb07bfe746
Pierre Rineau <pierre.rineau@makina-corpus.com>
2017-10-26 15:38:41 +0200

    index.php DrupalKernel micro optimization with precomputed app root

--- web/index.php
+++ web/index.php
@@ -1,10 +1,13 @@
 <?php
 use Drupal\Core\DrupalKernel;
+use MakinaCorpus\Lannion\Lannion;
 use Symfony\Component\HttpFoundation\Request;
+
+define('APP_ROOT', Lannion::getProjectRoot());
 
 $autoloader = require_once dirname(__DIR__).'/vendor/autoload.php';
 
-$kernel = new DrupalKernel('prod', $autoloader);
+$kernel = new DrupalKernel('prod', $autoloader, true, APP_ROOT);
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
